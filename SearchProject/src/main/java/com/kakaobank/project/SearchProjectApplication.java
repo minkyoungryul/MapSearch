6aff8193619eb118ef05528377f7ebbd1678a2ad
minkr <minkr3321@naver.com>
2020-09-17 00:24:24 +0900

    기본 세팅 및 검색 화면

---
+++ SearchProject/src/main/java/com/kakaobank/project/SearchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kakaobank.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchProjectApplication.class, args);
+	}
+
+}
